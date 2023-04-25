b54a3cd4b54e35ec02a68c6960b3b15429414599
Mauricio CR <mauriciocr123@outlook.com>
2023-04-25 12:18:19 -0600

    cambios en baudrate

--- serialPort/serialPort.js
+++ serialPort/serialPort.js
@@ -1,7 +1,7 @@
 // Conexión Serial
 import { SerialPort, DelimiterParser } from "serialport";
 import { BrowserWindow } from 'electron';
-const baudRateValue = 1200;
+const baudRateValue = 9600;
 
 export class Connection {
     constructor(port) {
