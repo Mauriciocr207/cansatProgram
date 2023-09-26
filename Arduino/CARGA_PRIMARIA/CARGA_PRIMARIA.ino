2e977a7252d603d4e0920680b689c0246699d971
Mauricio CR <mauriciocr123@outlook.com>
2023-09-25 19:54:56 -0600

    se añade script carga primaria

--- Arduino/CARGA_PRIMARIA/CARGA_PRIMARIA.ino
+++ Arduino/CARGA_PRIMARIA/CARGA_PRIMARIA.ino
@@ -43,7 +43,6 @@ void setup() {
   byte status = mpu.begin();
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);
-  while(status!=0){ } // stop everything if could not connect to MPU6050
   Serial.println(F("Calculating offsets, do not move MPU6050"));
   delay(1000);
   mpu.calcOffsets(true,true); // gyro and accelero
